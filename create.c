4856dd56ff6a838fa60234ed56ff5d65e17e5be2
linnaid <jiaguoyangyang@qq.com>
2024-11-24 21:26:34 +0800

    create

---
+++ create.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//创建结构体
+typedef struct node
+{
+    int data;
+    struct node *next;
+}Node;
+
+//创建表头
+Node *creatlist()
+{
+    Node *headNode = (Node *)malloc(sizeof(Node));
+    if(headNode == NULL)
+    exit(1);
+    //headNode->data = 1
+    //注释头节点数据为1的目的：保持头节点为空表头，不被初始化为1（数据若需要被使用，则必须初始化）
+    headNode->next = NULL;
+    return headNode;
+}
+
+//创建节点
+Node *creatNode(int data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if(newNode == NULL)
+    exit(1);
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
